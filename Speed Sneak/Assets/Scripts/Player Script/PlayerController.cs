33636d287ae7d3f8ffd72b179213109c6fa45ca0
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2022-07-08 13:49:11 -0400

    Added Player Controls And Animation
    Added player controls to allow player movement and added "run and aim" animation.

--- Speed Sneak/Assets/Scripts/Player Script/PlayerController.cs
+++ Speed Sneak/Assets/Scripts/Player Script/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // TODO: Work on allowing the player to move and apply animation accordingly.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.Play("RunAndAim");
+            transform.Translate(Vector3.forward * Time.deltaTime * (Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift) ? 20 : 1));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.Play("RunAndAim");
+            transform.Translate(Vector3.back * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -1);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, 1);
+        }
     }
 }
