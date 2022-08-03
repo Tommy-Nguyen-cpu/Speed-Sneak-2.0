b166c3ff69ea19b9006e61d0266bc6f9cd7e8419
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2022-08-03 14:27:05 -0400

    Updated GridScript To Generate NPC and Player
    Updated grid script to generate player and NPC.

--- Speed Sneak/Assets/Scripts/PCG_Beta_Scripts/GridScript.cs
+++ Speed Sneak/Assets/Scripts/PCG_Beta_Scripts/GridScript.cs
@@ -35,6 +35,10 @@ public class GridScript : MonoBehaviour
     public GameObject BasicMob;
     public GameObject PatrolAiMob;
 
+    // TOMMY
+    // PCG will generate player at start position of map.
+    public GameObject Player;
+
   //  bool doesSide = false;
 
     void Start()
@@ -73,6 +77,11 @@ public class GridScript : MonoBehaviour
     void PathClear(){
         //Initial Position starts in the middle of the bottom row
         int iteration = roomFar/2;
+
+        // TOMMY
+        // Generate player at midpoint at beginning of map.
+        Instantiate(Player, new Vector3((iteration*1f+1f), 1f, 0f), Quaternion.identity);
+
         //while still within the vertical bounds of the grid, continue the algorithm.
         while (iteration < (roomFar*roomSide)) {
 
@@ -83,7 +92,18 @@ public class GridScript : MonoBehaviour
         pathVal++; //used for dictionaries   
         //STORE VALUES TO DICTIONARIES
         Vector3 Position = new Vector3 (1.1f*(iteration % roomFar), 0f, 1.1f*(iteration / roomFar));
+
+
+
+
+        // TOMMY
+        // Same thing, I just made it so that the Y position is at -1.1 instead of 0.
         RouteList.Add(pathVal, Position); //stores locationt to entry
+
+
+
+
+
         RouteRowPos.Add(pathVal, iteration); //stores iteration value to entry
         RouteDir.Add(pathVal, lastDir);
 
@@ -180,7 +200,7 @@ public class GridScript : MonoBehaviour
             //but the hallway is still 6 blocks long total, so we need to offset it by one!
             char TileDir = RouteDir[midPoint];
             int rowPos = RouteRowPos[midPoint];
-            Vector3 AiPos = (RouteList[midPoint]);
+            Vector3 AiPos = new Vector3(RouteList[midPoint].x, 1f, RouteList[midPoint].z);
 
             bool BoundCheckTop = ((RouteRowPos[entry.Key] % roomSide) > 0 && RouteRowPos[entry.Key] % roomSide < roomSide-1);
             bool BoundCheckBottom = ((RouteRowPos[entry.Value] % roomSide) > 0 && RouteRowPos[entry.Value] % roomSide < roomSide-1);
@@ -260,9 +280,20 @@ public class GridScript : MonoBehaviour
             //ensures these sequences will not spawn at the start of the map.
             if (BottomPos > (roomSide*4)){
                 if (BottomDir == 'L' && (BottomPos % roomSide) > 2 && (TopPos % roomSide) > 2) {
-                     Vector3 rotationVectorL = new Vector3(0, 90, 0);
+
+
+
+                    // TOMMY
+                    // If the NPC is spawned at the bottom left of the loop, I want it to face to the right so it can rotate counter clockwise.
+                     Vector3 rotationVectorL = new Vector3(0, -90, 0);
                     Quaternion rotation = Quaternion.Euler(rotationVectorL);
-                    Instantiate(PatrolAiMob,BottomGeo,rotation);    
+                    GameObject instantiatedGuard = Instantiate(PatrolAiMob,BottomGeo,rotation);
+                     // TOMMY
+                    // Passes rotation direction to let NPC know which way to rotate.
+                    instantiatedGuard.GetComponent<AnimContr>().rotationDirection = 'R';  
+
+
+
                     GameObject.Destroy(Row[BottomPos-1]);
                     GameObject.Destroy(Row[BottomPos-2]);
                     GameObject.Destroy(Row[BottomPos-3]);
@@ -279,9 +310,25 @@ public class GridScript : MonoBehaviour
                     Debug.Log(entry.Value + " " + Bottom + " " + BottomPos);
      //               doesSide = true;
                 } else if (BottomDir == 'R' && (BottomPos % roomSide) < (roomSide - 2) && (TopPos % roomSide) < (roomSide - 2)) {
-                    Vector3 rotationVectorR = new Vector3(0, 270, 0);
+
+
+
+
+                    // TOMMY
+                    // If the NPC is spawned at the bottom right of the loop, I want it to face to the right so it can rotate clockwise.
+                    Vector3 rotationVectorR = new Vector3(0, 90, 0);
+
                     Quaternion rotation = Quaternion.Euler(rotationVectorR);
-                    Instantiate(PatrolAiMob,BottomGeo,rotation);    
+                    GameObject instantiatedGuard = Instantiate(PatrolAiMob,BottomGeo,rotation);
+
+                    // TOMMY
+                    // Pass rotation direction instruction to let NPC know which direction to rotate in.
+                    instantiatedGuard.GetComponent<AnimContr>().rotationDirection = 'L';
+
+
+
+
+
                     GameObject.Destroy(Row[BottomPos+1]);
                     GameObject.Destroy(Row[BottomPos+2]);
                     GameObject.Destroy(Row[BottomPos+3]);
