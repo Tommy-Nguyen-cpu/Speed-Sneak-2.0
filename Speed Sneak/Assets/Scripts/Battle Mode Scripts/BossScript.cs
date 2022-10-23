6a16646afec639e46345a161f4ef85a5486c7d64
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2022-10-23 11:30:36 -0400

    Delayed Upward Obstacle
    Delayed the activation of the upward obstacle until AFTER the boss appears (in idle mode).

--- Speed Sneak/Assets/Scripts/Battle Mode Scripts/BossScript.cs
+++ Speed Sneak/Assets/Scripts/Battle Mode Scripts/BossScript.cs
@@ -25,12 +25,6 @@ public class BossScript : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Speed Sneak/Assets/Scripts/Battle Mode Scripts/World Scripts/BattleWorldScript.cs
+++ Speed Sneak/Assets/Scripts/Battle Mode Scripts/World Scripts/BattleWorldScript.cs
@@ -7,6 +7,7 @@ public class BattleWorldScript : MonoBehaviour
 {
     private float timer = 0f;
     public GameObject player;
+    public Animator bossAnimation;
     public GameObject Indicator;
     private List<GameObject> indicators = new List<GameObject>();
     // Start is called before the first frame update
@@ -24,21 +25,24 @@ public class BattleWorldScript : MonoBehaviour
     void Update()
     {
         // TODO: Find a way to randomize the obstacle the player will face. Should use timer.
-        timer += Time.deltaTime;
-        if(timer > 2)
+        if (bossAnimation.GetCurrentAnimatorStateInfo(0).IsName("DroneGuard|Idle"))
         {
-            foreach(var myIndicator in indicators.Where(thisIndicator => !thisIndicator.activeInHierarchy))
+            timer += Time.deltaTime;
+            if (timer > 2)
             {
-                int xNegative = (Random.value > .5) ? -1 : 1;
-                int zNegative = (Random.value > .5) ? -1 : 1;
-                float xDifference = xNegative * Random.value * 10;
-                float zDifference = zNegative * Random.value * 10;
-                float playerX = player.transform.position.x;
-                float playerZ = player.transform.position.z;
-                myIndicator.transform.position = new Vector3(playerX + xDifference, myIndicator.transform.position.y, playerZ + zDifference);
-                myIndicator.SetActive(true);
+                foreach (var myIndicator in indicators.Where(thisIndicator => !thisIndicator.activeInHierarchy))
+                {
+                    int xNegative = (Random.value > .5) ? -1 : 1;
+                    int zNegative = (Random.value > .5) ? -1 : 1;
+                    float xDifference = xNegative * Random.value * 10;
+                    float zDifference = zNegative * Random.value * 10;
+                    float playerX = player.transform.position.x;
+                    float playerZ = player.transform.position.z;
+                    myIndicator.transform.position = new Vector3(playerX + xDifference, myIndicator.transform.position.y, playerZ + zDifference);
+                    myIndicator.SetActive(true);
+                }
+                timer = 0f;
             }
-            timer = 0f;
         }
     }
 }
